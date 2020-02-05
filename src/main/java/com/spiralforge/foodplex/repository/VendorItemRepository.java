f88f3c99cd1df4aadfd1ea5a0f099ec13bea6e7a
User1 <User1@LP-5CD9176M5C.HCLT.CORP.HCL.IN>
2020-02-05 18:31:47 +0530

    Adding vendorItem Details

---
+++ src/main/java/com/spiralforge/foodplex/repository/VendorItemRepository.java
@@ -0,0 +1,11 @@
+package com.spiralforge.foodplex.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spiralforge.foodplex.entity.VendorItem;
+
+@Repository
+public interface VendorItemRepository extends JpaRepository<VendorItem, Integer>{
+	
+}
