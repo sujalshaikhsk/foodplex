2accb3c5cec0bb1e28ba9dfc7f40548ca0f69613
Sujal Shaikh <sujalshaikhsk@gmail.com>
2020-02-05 13:51:38 +0530

    order and item repository created

--- src/main/java/com/spiralforge/foodplex/repository/UserRepository.java
+++ src/main/java/com/spiralforge/foodplex/repository/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.spiralforge.foodplex.entity.User;
-import com.spiralforge.foodplex.entity.Vendor;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
